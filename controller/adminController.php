e533b2c8a483fea5303b78a6054787f0eab0bb00
guixou <guillaumejuju@live.fr>
2021-12-13 14:04:31 +0100

    13/12
    correction lien

--- controller/adminController.php
+++ controller/adminController.php
@@ -10,7 +10,7 @@ function admin($notification){
     session_start();
     //on est sur une page où on doit être identifié -> si la variable session n'existe pas -> rediriger l'utilisateur vers la page de login
     if (!isset($_SESSION['user'])) {
-        header('Location: Accueil');
+        header('Location: Login');
         exit;
     }
 
